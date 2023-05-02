cd54f7a47d1d726fc90238568e8a0bcb7497ced0
mochbale <mochammadiqbal14081999@gmail.com>
2023-05-02 13:50:12 +0700

    sudah kelar

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { MdPlaylistAdd } from 'react-icons/md';
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
@@ -12,14 +13,19 @@ function TodoList() {
   };
 
   const handleDeleteTodo = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
+    const task = todos[index];
+    const confirmDelete = window.confirm(`Apakah anda yakin sudah menyelesaikan task tersebut: "${task}"?`); // menampilkan dialog konfirmasi dengan isi tugas
+    if (confirmDelete) {
+      const newTodos = [...todos];
+      newTodos.splice(index, 1);
+      setTodos(newTodos);
+    }
   };
+  
 
   return (
     <div>
-      <h1 className="title">To Do List</h1>
+      <h1 className="title"><MdPlaylistAdd className="icon" />To Do List</h1>
       <input type="text" className="todo-input" value={todoInput} onChange={(e) => setTodoInput(e.target.value)} />
       <button className="add" onClick={handleAddTodo}>Add</button>
       <ul className="todo-list">
@@ -30,6 +36,11 @@ function TodoList() {
           </li>
         ))}
       </ul>
+
+      <footer className="footer">
+      <p>Created by <a href="https://www.instagram.com/michaelwilliam_14/">Michael William</a></p>
+      
+    </footer>
     </div>
   );
 }
